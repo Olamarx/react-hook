aef1a53ac3e49ed274d938a25168403f32913325
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-10-16 20:32:30 +0100

    Start the dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 
 const items = [
   {
@@ -17,11 +18,33 @@ const items = [
   },
 ]
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'This is a Green color',
+    value: 'green'
+  },
+  {
+    label: 'Showing an orange Color',
+    value: 'orange'
+  },
+]
+
 const App = () => {
+  const [ selected, setSelected ] = useState(options[0])
+
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+      selected={selected}
+      options={options}
+      onSelectedChange={setSelected}
+      />
     </div>
   )
 }
