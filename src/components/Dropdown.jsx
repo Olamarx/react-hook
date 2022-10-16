19b4be140925d769170acabe9a0cd52a386e6bf6
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-10-16 21:32:02 +0100

    Finalize dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false) 
-
+    const ref = useRef();
     useEffect(() => {
-        document.addEventListener('click', () => {
+        const onBodyClick =  (event) => {
+            if (ref.current.contains(event.target)) {
+                return
+            }
             setOpen(false)
-        }, {capture: true})
+        }
+            
+            document.body.addEventListener('click', onBodyClick)
+        
+            return () => {
+                document.body.removeEventListener('click', onBodyClick)
+            }
     }, [])
 
     const renderedOptions = options.map((option) => {
@@ -24,10 +33,9 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 )
     })
 
-
   return (
     <div>
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label htmlFor="" className="label">Select a Color</label>
                 <div
